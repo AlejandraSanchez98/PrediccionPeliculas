c8d74999bc2f3a13dd4917591afa27ef74dcfbe4
JLGallardoV <Gallardo.vaca@gmail.com>
2019-07-19 00:39:01 -0500

    BACK-END PROGRESO....
    -Falta el algoritmo araña.
    -Falta la BD

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 //CREACION DE INTERFACES
-interface Ipeliculas {
-idPelicula: number,
-tituloPelicula: string,
-generoPelicula: string,
-fechaEstrenoPelicula: string,
-resumenPelicula: string,
-paisOrigenPelicula: string,
-duracionPelicula: string,
-calificacionPelicula: number
+interface IInformacionPeliculas{
+  idPelicula: number;
+  tituloPelicula: string;
+  imagenPelicula: string;
+  generoPelicula: string;
+  fechaEstrenoPelicula: string;
+  resumenPelicula: string;
+  paisOrigenPelicula: string;
+  duracionPelicula: string;
+  calificacionFinalPelicula: number;
 }
 
 @Component({
@@ -19,37 +21,18 @@ calificacionPelicula: number
 })
 export class HomeComponent implements OnInit {
   //PROPIEDADES:
-  public valoracionDirector:number;
-  public valoracionGuionista:number;
-  public valoracionActores:number;
-  public valoracionProductor:number;
-  public arregloPelicula:Ipeliculas[];
+  public arregloPelicula: IInformacionPeliculas[];
+  constructor(public router: Router) {
 
-  constructor() {
-    this.valoracionDirector = 5;
-    this.valoracionGuionista = 5;
-    this.valoracionActores = 10;
-    this.valoracionProductor = 10;
-
-    this.arregloPelicula = [];
   }
-  //ESTE METODO PROMEDIA LAS VALORACIONES DE CADA UNO DE LOS ELEMENTOS INVOLUCRADOS
-  public promediar(): number{
-    let calificacionFinal: number = 0;
+  public navegarxPelicula(idPelicula:number): void
+  {
+    this.router.navigate(['/informacion-peliculas'],{queryParams:{idPelicula:idPelicula}});
+  }
 
-    calificacionFinal = ((this.valoracionDirector +  this.valoracionGuionista + this.valoracionActores + this.valoracionProductor) /4);
-
-    return calificacionFinal;
-  }//fin promediar
-
-  //MUESTRA LOS DATOS DE LA PELICULA
-  public MostrarPelicula(){
-    let calificacionFinal: number = this.promediar()
-    this.arregloPelicula.push({idPelicula:1, tituloPelicula:'DragonBall',generoPelicula:'anime',fechaEstrenoPelicula:"02-03-2020",resumenPelicula:"La mejor pelicula del mundo", paisOrigenPelicula:'Japon',duracionPelicula:'2hrs 29min', calificacionPelicula:calificacionFinal})
-  }//fin MostrarPelicula
 
   ngOnInit() {
-    this.MostrarPelicula();
+    /*PRACTICAMENTE SERIA AQUI UN SELECT * FROM DE PELICULAS, UNICAMENTE DEJANDO LA IMAGEN*/
   }
 
 }
--- src/app/informacion-peliculas/informacion-peliculas.component.ts
+++ src/app/informacion-peliculas/informacion-peliculas.component.ts
@@ -1,13 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-
+interface IInformacionPeliculas{
+  idPelicula: number;
+  tituloPelicula: string;
+  imagenPelicula: string;
+  generoPelicula: string;
+  fechaEstrenoPelicula: string;
+  resumenPelicula: string;
+  paisOrigenPelicula: string;
+  duracionPelicula: string;
+  calificacionFinalPelicula: number;
+}
 @Component({
   selector: 'app-informacion-peliculas',
   templateUrl: './informacion-peliculas.component.html',
   styleUrls: ['./informacion-peliculas.component.scss']
 })
 export class InformacionPeliculasComponent implements OnInit {
+  //PROPIEDADES:
+  arregloPelicula: IInformacionPeliculas[];
+  public valoracionDirector:number;
+  public valoracionGuionista:number;
+  public valoracionActores:number;
+  public valoracionProductor:number;
+  constructor() {
+    this.valoracionDirector = 5;
+    this.valoracionGuionista = 5;
+    this.valoracionActores = 10;
+    this.valoracionProductor = 10;
+    this.arregloPelicula = [];
+  }
 
-  constructor() { }
+  //ESTE METODO PROMEDIA LAS VALORACIONES DE CADA UNO DE LOS ELEMENTOS INVOLUCRADOS
+  public promediar(): number{
+    let calificacionFinal: number = 0;
+
+    calificacionFinal = ((this.valoracionDirector +  this.valoracionGuionista + this.valoracionActores + this.valoracionProductor) /4);
+
+    return calificacionFinal;
+  }//fin promediar
+
+
+    //MUESTRA LOS DATOS DE LA PELICULA
+    /*public MostrarPelicula(){
+      let calificacionFinal: number = this.promediar()
+      this.arregloPelicula.push({idPelicula:1, tituloPelicula:'DragonBall',generoPelicula:'anime',fechaEstrenoPelicula:"02-03-2020",resumenPelicula:"La mejor pelicula del mundo", paisOrigenPelicula:'Japon',duracionPelicula:'2hrs 29min', calificacionPelicula:calificacionFinal})
+    }/*/
 
   ngOnInit() {
   }
