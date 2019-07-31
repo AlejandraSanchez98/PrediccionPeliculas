defdd7d6e499b888261f312e9b569899f2e32e81
Francis <215030210@uppenjamo.edu.mx>
2019-07-31 11:47:33 -0500

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,14 @@ import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
 import {IInformacionPeliculas} from '../api.service';
 
+// const para WebScraping
+const cheerio = require ('cheerio');
+const request = require('request');
+const fs = require('fs');
+const arrayPuntuaje = [];
+const arrayTitulo = [];
+const arrayImg = [];
+
 
 @Component({
   selector: 'app-home',
@@ -35,6 +43,29 @@ export class HomeComponent implements OnInit {
         console.log("sigue intentado!!");
       }
     );
+  }
+
+  public webScraping():void
+  {
+    var url = 'https://www.filmaffinity.com/mx/rdcat.php?id=upc_th_mx';
+    request(url,function (error, response, html) { // solo es un callback
+      if(!error && response.statusCode == 200)
+      {
+          var $ = cheerio.load(html); // carga todo el cuerpo o body de la Pagina a buscar
+          $('div.avg-rating').each(function(i,element) { // seleccion de etiquetas de interes en esta pagina fue esta
+              var puntuaje =$(element).text(); //variable para almacenar los elementos buscados en este caso El Puntuaje de la Pelicula
+              if(puntuaje!='')
+                  console.log((i + 1) + ' .- ' + puntuaje); // Muestra todos los valores encontrdos bajo esas etiquetas "span.note"
+                  arrayPuntuaje.push(puntuaje); // arreglo para almacenar los puntuaje de las peliculas
+          });
+           
+      }
+  
+      //Esta parte la puedes meter dentro de un ciclo para que relacione el puntuaje con su respesctivo Nombre de Pelicula
+      console.log(arrayImg);
+      console.log(arrayTitulo[0]+arrayPuntuaje[0]+arrayImg[0]);
+  });
+
   }
 
 
