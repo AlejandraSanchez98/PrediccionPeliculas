0d19673e78745a172498a34ac187cb09b56cf30c
Alejandra Sanchez <anasanchezarias98@gmail.com>
2019-08-15 01:39:20 -0500

    Diseño de informacion de peliculas- Concluido

--- src/app/agregar-informacion/agregar-informacion.component.ts
+++ src/app/agregar-informacion/agregar-informacion.component.ts
@@ -11,9 +11,39 @@ import {IInformacionPeliculas} from '../api.service';
 })
 export class AgregarInformacionComponent implements OnInit {
   public arregloPeliculas: IInformacionPeliculas[];
+  public frmPeliculas: FormGroup;
+  public formValid:Boolean=false;
   constructor( public formBuilder: FormBuilder, public router: Router, public API:ApiService) { 
     this.arregloPeliculas = [];
+
+    this.frmPeliculas = this.formBuilder.group({
+      tituloPelicula:["",Validators.required],
+      generoPelicula:["",Validators.required],
+      fechaEPelicula:["",Validators.required],
+      resumenPelicula:["",Validators.required],
+      imagenPelicula:["",Validators.required],
+      paisPelicula:["",Validators.required],
+      duracionPelicula:["",Validators.required],
+      calificacionPelicula:["",Validators.required]
+    });
   }
+
+  /*public guardarPelicula(){
+    let tituloPeliculaForm = this.frmPeliculas.get('tituloPelicula').value;
+    let generoPeliculaForm = this.frmPeliculas.get('generoPelicula').value;
+    let costoForm = this.frmViajes.get('costo_total').value;
+    this.route.queryParams.subscribe((params: IInformacionPeliculas) => {
+    return this.API.guardarPelicula(tituloPelicula, generoPelicula, fechaEPelicula, resumenPelicula, imagenPelicula,paisPelicula,duracionPelicula, calificacionPelicula).subscribe(
+      (success:any)=>{
+        this.arregloPeliculas = success;
+        console.log("Pelicula: "+ JSON.stringify(this.arregloPeliculas));
+      },
+      (error)=>{
+        console.log("SIGUE INTENTANDO ANIMAL: "+ error);
+      }
+    );
+  }
+);}*/
 
   ngOnInit() {
   }
